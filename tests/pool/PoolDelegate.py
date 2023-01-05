2ab29629e6c35a922d8d86ca3ce0919afa38edf7
kibagateaux <kibagateaux@gmail.com>
2023-01-05 18:17:31 -0500

    add tests

---
+++ tests/pool/PoolDelegate.py
@@ -0,0 +1,28 @@
+import boa
+from boa.vyper.contract import BoaError
+from hypothesis import given, settings
+from hypothesis import strategies as st
+from hypothesis.stateful import RuleBasedStateMachine, run_state_machine_as_test, rule, invariant
+from datetime import timedelta
+
+
+
+# TODO for debt dao
+@given(
+    oracle_price=st.integers(min_value=2000 * 10**18, max_value=4000 * 10**18),
+    n1=st.integers(min_value=1, max_value=50),
+    dn=st.integers(min_value=0, max_value=49),
+    deposit_amount=st.integers(min_value=10**12, max_value=10**20),
+    init_trade_frac=st.floats(min_value=0.0, max_value=1.0),
+    p_frac=st.floats(min_value=0.1, max_value=10)
+)
+@settings(max_examples=500, deadline=timedelta(seconds=1000))
+def test_owner_fees_burned_on_impairment(
+    price_oracle, amm, accounts, collateral_token, borrowed_token, admin,
+    oracle_price, n1, dn, deposit_amount, init_trade_frac, p_frac
+):
+    user = accounts[0]
+    with boa.env.prank(admin):
+        amm.set_fee(0)
+        price_oracle.set_price(oracle_price)
+   
---
+++ tests/pool/conftest.py
@@ -0,0 +1,19 @@
+import boa
+import pytest
+from math import log
+
+boa.interpret.set_cache_dir()
+boa.reset_env()
+
+
+@pytest.fixture(scope="session")
+def pool(admin, asset_token):
+    # deploy a pool contract and connect it
+    return boa.load('contracts/PoolDelegate.vy', admin, asset_token, "Test", "TEST", {
+        'performance': '1000',
+        'deposit': '0',
+        'withdraw': '0',
+        'flash': '15',
+        'collector': '100',
+        'referral': '50',
+    })
